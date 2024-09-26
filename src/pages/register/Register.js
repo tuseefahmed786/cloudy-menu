8aca26ba30096d55cfdbfeff9e7c41c32bfdc61f
tuseefahmed786 <86749819+tuseefahmed786@users.noreply.github.com>
2024-09-26 20:33:29 +0500

    Update Register.js

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -9,7 +9,7 @@ function Register() {
         e.preventDefault()
         console.log(email,password)
        try {
-        const registered = await axios.post("http://localhost:3002/signup",{
+        const registered = await axios.post("https://emenu-sandy.vercel.app/signup",{
             email,
             password
         },{
