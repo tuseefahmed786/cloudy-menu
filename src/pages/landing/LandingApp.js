d82e51dc966e01f0161cf330537c59ea072bedfc
tuseefahmed786 <86749819+tuseefahmed786@users.noreply.github.com>
2024-12-20 23:44:02 +0500

    Update LandingApp.js

--- src/pages/landing/LandingApp.js
+++ src/pages/landing/LandingApp.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HeroSectionLan from './HeroSectionLan';
 import HeaderLan from './HeaderLan';
 import PricingLan from './PricingLan';
 import FooterLanding from './FooterLanding';
 
 function LandingApp() {
+    useEffect(() => {
+    if (window.location.hostname === 'www.qr.cloudymenu.com') {
+      // Redirect to 'https://www.cloudymenu.com'
+      window.location.href = 'https://www.cloudymenu.com';
+    }
+  }, []);
   return (
     <div className='bg-[#c8ffdd]'>
       <HeroSectionLan />
