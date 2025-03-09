fc44688d3c10da4cc064dfa8f356271090f1aa9e
tuseefahmed786 <86749819+tuseefahmed786@users.noreply.github.com>
2025-03-09 22:46:18 +0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosApi = axios.create({
     //  baseURL: 'https://menuserver-eight.vercel.app', // Set your base URL here
-    baseURL: 'http://localhost:3002',
+    baseURL: 'https://menuserver-eight.vercel.app',
 });
 
 
